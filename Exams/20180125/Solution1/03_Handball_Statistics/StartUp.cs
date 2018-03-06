b18a41f3f5331b733ddbae0395a9a3c79d59f001
inser788 <inser7@abv.bg>
2018-03-06 12:06:10 +0200

    update 03. HadBall_Statistics to 100/100

--- Exams/20180125/Solution1/03_Handball_Statistics/StartUp.cs
+++ Exams/20180125/Solution1/03_Handball_Statistics/StartUp.cs
@@ -9,27 +9,38 @@ public class StartUp
         public string Name { get; set; }
         public int GoalsHome { get; set; }
         public int GoalsAway { get; set; }
+        public int TotalGoals { get; set; }
         public int Wins { get; set; }
         public List<string> Opponents { get; set; }
-        
-     }
+
+    }
 
     static void Main()
     {
-        var listOfTeams = new SortedDictionary<string, Country>();
+        var listOfTeams = new Dictionary<string, Country>();
 
         while (true)
         {
             string input = Console.ReadLine();
             if (input == "stop")
                 break;
-            string[] dataTeams = input.Split("|".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+            string[] dataTeams = input.Split(new[] {" | "}, StringSplitOptions.RemoveEmptyEntries);
 
             listOfTeams = GetTeamsList(listOfTeams, dataTeams);
+
+        }
+
+        listOfTeams = listOfTeams.OrderByDescending(x => x.Value.Wins).ThenBy(x => x.Value.Name)
+            .ToDictionary(k => k.Key, v => v.Value);
+        foreach (var team in listOfTeams.Values)
+        {
+            Console.WriteLine(team.Name);
+            Console.WriteLine($"- Wins: {team.Wins}");
+            Console.WriteLine($"- Opponents: " + string.Join(", ", team.Opponents));
         }
     }
 
-    static SortedDictionary<string, Country> GetTeamsList(SortedDictionary<string, Country> listOfTeams,
+    static Dictionary<string, Country> GetTeamsList(Dictionary<string, Country> listOfTeams,
         string[] dataTeams)
     {
         string firstTeam = dataTeams[0].Trim();
@@ -38,27 +49,48 @@ public class StartUp
         string secondVsFirstResult = dataTeams[3].Trim();
         listOfTeams = GetTeamData(listOfTeams, firstTeam, firstVsSecondResult, secondVsFirstResult, secondTeam);
         listOfTeams = GetTeamData(listOfTeams, secondTeam, secondVsFirstResult, firstVsSecondResult, firstTeam);
-
+        if (listOfTeams[firstTeam].TotalGoals > listOfTeams[secondTeam].TotalGoals)
+        {
+            listOfTeams[firstTeam].Wins++;
+        }
+        else if (listOfTeams[secondTeam].TotalGoals > listOfTeams[firstTeam].TotalGoals)
+        {
+            listOfTeams[secondTeam].Wins++;
+        }
+        else
+        {
+            if (listOfTeams[firstTeam].GoalsAway > listOfTeams[secondTeam].GoalsAway)
+            {
+                listOfTeams[firstTeam].Wins++;
+            }
+            else if (listOfTeams[secondTeam].GoalsAway > listOfTeams[firstTeam].GoalsAway)
+            {
+                listOfTeams[secondTeam].Wins++;
+            }
+        }
 
         return listOfTeams;
     }
 
-    static SortedDictionary<string, Country> GetTeamData(SortedDictionary<string, Country> listTeams, string teamName, string firstResult,
+    static Dictionary<string, Country> GetTeamData(Dictionary<string, Country> listTeams, string teamName,
+        string firstResult,
         string secondResult, string opponent)
     {
         if (!listTeams.ContainsKey(teamName))
         {
             listTeams[teamName] = new Country();
-            listTeams[teamName].Name = teamName;
             listTeams[teamName].Opponents = new List<string>();
         }
 
+        listTeams[teamName].Name = teamName;
         listTeams[teamName].GoalsHome = int.Parse(firstResult.Split(":".ToCharArray()).First());
         listTeams[teamName].GoalsAway = int.Parse(secondResult.Split(":".ToCharArray()).Last());
-        if (!listTeams[teamName].Opponents.Contains(opponent))
-        {
-            listTeams[teamName].Opponents.Add(opponent);
-        }
+        listTeams[teamName].TotalGoals = 0;
+        listTeams[teamName].TotalGoals += listTeams[teamName].GoalsHome + listTeams[teamName].GoalsAway;
+
+        listTeams[teamName].Opponents.Add(opponent);
+        listTeams[teamName].Opponents.Sort();
+
 
         return listTeams;
     }
