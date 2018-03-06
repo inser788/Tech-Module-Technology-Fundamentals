bde604e1b0eef82b7f8ded612d7febda6e251cc7
inser788 <inser7@abv.bg>
2018-03-07 00:58:17 +0200

    add new Exam/20180304/ 2/4 solutions 200/400 points,TODO next 2 problems

---
+++ Exams/20180304/20180304-Exam/01_Padawan_Equipment/StartUp.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+public class StartUp
+{
+    static void Main()
+    {
+        decimal allMoney = decimal.Parse(Console.ReadLine());
+        int countStudents = int.Parse(Console.ReadLine());
+        decimal sabersPrice = decimal.Parse(Console.ReadLine());
+        decimal robesPrice = decimal.Parse(Console.ReadLine());
+        decimal beltsPrice = decimal.Parse(Console.ReadLine());
+
+        int sabersCount = (int)Math.Ceiling(countStudents*1.1);
+        int robesCount = countStudents;
+        int beltsCount = countStudents-(countStudents / 6);
+
+        decimal totalPrice = sabersPrice * sabersCount + robesPrice * robesCount + beltsPrice * beltsCount;
+
+        decimal diffrences = allMoney - totalPrice;
+        if (diffrences>=0)
+        {
+            Console.WriteLine($"The money is enough - it would cost {totalPrice:f2}lv.");
+        }
+        else
+        {
+            Console.WriteLine($"Ivan Cho will need {-diffrences:f2}lv more.");
+        }
+    }
+}
+
---
+++ Exams/20180304/20180304-Exam/02_Kamino_Factory/StartUp.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class StartUp
+{
+    static void Main()
+    {
+        int lengthSequance = int.Parse(Console.ReadLine());
+        var listOfAllSequances = new List<string>();
+        var listOfBestPosition = new List<int>();
+        while (true)
+        {
+            string input = Console.ReadLine();
+            if (input == "Clone them!") break;
+            var allDigits = new string(input.Where(n => char.IsDigit(n)).ToArray());
+
+            int longestSeqLength = 1;
+            int longestSeqStart = 0;
+            int currentSeqLength = 1;
+            int currentSeqStart = 0;
+
+            for (int i = 1; i < allDigits.Length; i++)
+            {
+                if (allDigits[i] == allDigits[i - 1] && allDigits[i] == '1')
+                {
+                    currentSeqLength++;
+
+                    if (currentSeqLength > longestSeqLength)
+                    {
+                        longestSeqLength = currentSeqLength;
+                        longestSeqStart = currentSeqStart;
+                    }
+                }
+                else
+                {
+                    currentSeqLength = 1;
+                    currentSeqStart = i;
+                }
+            }
+
+            listOfAllSequances.Add(allDigits);
+            listOfBestPosition.Add(longestSeqStart);
+
+        }
+
+        var tempResult = new List<int>();
+        int minIndex = listOfBestPosition.Min();
+        int startPos = 0;
+        int startSum = 0;
+        int counter = -1;
+        for (int i = 0; i < listOfBestPosition.Count; i++)
+        {
+            if (listOfBestPosition[i] == minIndex)
+            {
+                tempResult.Add(i);
+                counter++;
+                if (listOfAllSequances.ElementAt(tempResult[counter]).Sum(x => int.Parse(x.ToString())) > startSum)
+                {
+                    startSum = listOfAllSequances.ElementAt(tempResult[counter]).Sum(x => int.Parse(x.ToString()));
+                    startPos = i;
+                }
+            }
+        }
+
+        if (tempResult.Count == 1)
+        {
+            Console.WriteLine($"Best DNA sample {startPos + 1} with sum: {startSum}.");
+            Console.WriteLine(string.Join(" ", listOfAllSequances.ElementAt(startPos).ToArray()));
+        }
+        else
+        {
+            Console.WriteLine($"Best DNA sample {startPos + 1} with sum: {startSum}.");
+            Console.WriteLine(string.Join(" ", listOfAllSequances.ElementAt(startPos).ToArray()));
+        }
+    }
+}
+
